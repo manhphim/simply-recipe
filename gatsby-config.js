99c6121806b4460fd9c2c09774157abde4748c1d
Minh Pham <110820517+manhphim1205@users.noreply.github.com>
2022-08-12 13:52:17 +0200

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,29 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "A simple recipe site",
+    author: "John Doe",
+  },
   plugins: [
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `styles`,
+    //     path: `${__dirname}/src/assets/css`,
+    //   },
+    // },
   ],
 }
