0fc3766f2134cca29fb272b130ac380f63a1b45e
Minh Pham <110820517+manhphim1205@users.noreply.github.com>
2022-08-12 09:18:27 +0200

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <Layout>
+    <h1>Home</h1>
+  </Layout>
 }
